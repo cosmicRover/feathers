08635fccd24ab6de162c0b858c4ff03d0046b893
jagnaudh <jagnaudh@gmail.com>
2020-03-29 17:05:13 -0400

    added onTap & Begin Button

--- lib/login_flow/login_view.dart
+++ lib/login_flow/login_view.dart
@@ -19,61 +19,69 @@ class LoginView extends StatelessWidget {
             width: width,
             fit: BoxFit.fill,
           ),
-
           Padding(
-            padding: const EdgeInsets.only(top:30.0, left: 22.0),
-            child: Text("Feathers,", style: TextStyle(
-              color: Color.fromRGBO(2, 13, 83, 1),
-              fontSize: 28.0
-            ),),
+            padding: const EdgeInsets.only(top: 30.0, left: 22.0),
+            child: Text(
+              "Feathers,",
+              style: TextStyle(
+                  color: Color.fromRGBO(2, 13, 83, 1), fontSize: 28.0),
+            ),
           ),
-
           Padding(
-            padding: const EdgeInsets.only(top:60.0, left: 22.0),
-            child: Text("Discover them all", style: TextStyle(
-                color: Color.fromRGBO(2, 13, 83, 1),
-                fontSize: 28.0
-            ),),
+            padding: const EdgeInsets.only(top: 60.0, left: 22.0),
+            child: Text(
+              "Discover them all",
+              style: TextStyle(
+                  color: Color.fromRGBO(2, 13, 83, 1), fontSize: 28.0),
+            ),
           ),
-          
           Padding(
-            padding: const EdgeInsets.only(top:150.0),
+            padding: const EdgeInsets.only(top: 150.0),
             child: Align(
               alignment: Alignment.bottomLeft,
               child: Image.asset("assets/login_assets/flower_gif.gif"),
             ),
           ),
-
           Padding(
-            padding: const EdgeInsets.only(top:80.0, right: 20.0),
+            padding: const EdgeInsets.only(top: 80.0, right: 20.0),
             child: Align(
               alignment: Alignment.topLeft,
-              child: Image.asset("assets/login_assets/bird.gif", height: 300.0,),
+              child: Image.asset(
+                "assets/login_assets/bird.gif",
+                height: 300.0,
+              ),
             ),
           ),
-          
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
-              padding: const EdgeInsets.only(bottom:8.0),
-              child: flrButton(),
-            )
-          )
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: GestureDetector(
+                child: flrButton(),
+                onTap: () => _viewModel.initLoginFlow,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
-  Widget flrButton(){
+
+  Widget flrButton() {
     return Container(
       height: 130.0,
-      child: FlareActor(
-        "assets/login_assets/play.flr",
-        fit: BoxFit.contain,
-        animation: "stand_by",
-        color: Color.fromRGBO(18, 44, 147, 1),
+      child: Stack(
+        children: <Widget>[
+          
+          FlareActor(
+            "assets/login_assets/play.flr",
+            fit: BoxFit.contain,
+            animation: "stand_by",
+            color: Color.fromRGBO(18, 44, 147, 1),
+          ),
+          Center(child: Text("Begin", style: TextStyle(color: Colors.white),),),
+        ],
       ),
     );
   }
-
 }
-
