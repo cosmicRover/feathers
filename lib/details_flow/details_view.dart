8a6714463cb8273503c3f615ecc7fcf5b2d3cd5e
jagnaudh <jagnaudh@gmail.com>
2020-03-30 03:59:11 -0400

    JSON DATA

--- lib/details_flow/details_view.dart
+++ lib/details_flow/details_view.dart
@@ -25,39 +25,183 @@ class _detailsViewState extends State<detailsView> {
     return Scaffold(
       backgroundColor: _colors.profileBlue,
       body: Stack(
-        
         children: <Widget>[
-          
-          
-          
-          
-          SizedBox(height: 40),
           ListView(
             children: <Widget>[
               FutureBuilder<Bird>(
-            future: futureBird,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.name, style: TextStyle(fontFamily: 'Gibson', fontSize: 40, color: Colors.black),);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return CircularProgressIndicator();
-            },
-          ),
-              
+                future: futureBird,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 58),
+                              child: Text(
+                                "${snapshot.data.name}",
+                                style: TextStyle(
+                                    fontSize: 29,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+                          Image.asset(
+                            "assets/detailsView_assets/jay.png",
+                            fit: BoxFit.fill,
+                            width: _width,
+                          ),
+                          Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Habitat",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.habitat}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
 
-              
-              
+                               Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Plumage",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.plumage}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
 
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  "assets/detailsView_assets/FullBackground.png",
-                  fit: BoxFit.fill,
-                  width: _width,
-                ),
+                               Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Nest",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.nest}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+
+                              Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Diet",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.diet}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+                              Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Lifespan",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.lifespan}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+
+                              Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Conservation Status",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.conservationStatus}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+                              Padding(
+                            padding:const EdgeInsets.only(left: 18, top: 18),
+                            
+                            child: Text("Fun facts",
+                              style: TextStyle(
+                                color: Color(0xFFF3D338),
+                                fontSize: 20,
+                              ))),
+                          Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.funFacts[0]}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+                              Padding(
+                              padding: const EdgeInsets.only(left: 18, top: 18),
+                              child: Text(
+                                "${snapshot.data.funFacts[1]}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
+                              )),
+
+                              
+
+                        SizedBox(height: 100,),
+                        ]));
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                },
               ),
+              // Align(
+              //   alignment: Alignment.center,
+              //   child: Image.asset(
+              //     "assets/detailsView_assets/FullBackground.png",
+              //     fit: BoxFit.fill,
+              //     width: _width,
+              //   ),
+              // ),
             ],
           ),
           Align(
@@ -68,7 +212,6 @@ class _detailsViewState extends State<detailsView> {
               width: _width,
             ),
           ),
-          
           Align(
             alignment: Alignment.bottomCenter,
             child: Image.asset(
@@ -79,6 +222,28 @@ class _detailsViewState extends State<detailsView> {
           )
         ],
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: Container(
+          height: 70.0,
+          width: 70.0,
+          child: FittedBox(
+            child: FloatingActionButton(
+              child: Icon(
+                Icons.arrow_downward,
+                size: 26.0,
+                color: _colors.menuBlue,
+              ),
+              onPressed: () {
+                // _showNavMenu(context);
+              },
+//              shape: RoundedRectangleBorder(
+//                  borderRadius: BorderRadius.all(Radius.circular(12.0))),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
@@ -96,6 +261,36 @@ class _detailsViewState extends State<detailsView> {
 //               return CircularProgressIndicator();
 //             },
 //           ),
-              
+
 //   );
 // }
+
+// ListView(
+//             children: <Widget>[
+//               FutureBuilder<Bird>(
+//                 future: futureBird,
+//                 builder: (context, snapshot) {
+//                   if (snapshot.hasData) {
+//                     return Text(
+//                       snapshot.data.name,
+//                       style: TextStyle(
+//                           fontFamily: 'Gibson',
+//                           fontSize: 40,
+//                           color: _colors.profileBlue),
+//                     );
+//                   } else if (snapshot.hasError) {
+//                     return Text("${snapshot.error}");
+//                   }
+//                   return CircularProgressIndicator();
+//                 },
+//               ),
+//               Align(
+//                 alignment: Alignment.center,
+//                 child: Image.asset(
+//                   "assets/detailsView_assets/FullBackground.png",
+//                   fit: BoxFit.fill,
+//                   width: _width,
+//                 ),
+//               ),
+//             ],
+//           )
